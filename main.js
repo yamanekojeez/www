9c2d89e22ecbca4a50f975f0effea11629adf496
yamanekojeez <yamaneko.jeez@gmail.com>
2020-02-08 17:19:16 +0900

    new file:   main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById("view_brs").innerHTML = browser();
+//ブラウザの判定
+function browser() {
+	var brs;
+	if (document.all) {
+		//Internet Explorer
+		brs = "Internet Explorer";
+	} else {
+		//その他のブラウザ
+		brs = "Internet Explorer以外のブラウザ";
+	}
+	return "あなたは<font color='#FF0000'>" + brs + "</font>を使っています。";
+}
+</script>
