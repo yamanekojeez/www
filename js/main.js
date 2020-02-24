b8814c047919775fa9c5bb1bdbc0c982ef8d5912
yamanekojeez <yamaneko.jeez@gmail.com>
2020-02-24 12:27:39 +0900

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-alert("Hello World!");
+/* alert("Hello World!"); */
+document.getElementById("view_brs").innerHTML = browser();
+function browser() {
+        let agent = window.navigator.userAegnt.toLowerCase();
+        if(agent.indexOf('msie') > -1) {
+            brs = 'InternetExplorer';
+        } else if(agent.indexOf('edge') > -1) {
+            brs = 'Edge';
+        } else if(agent.indexOf('chrome') > -1) {
+            brs = 'Edge';
+        } else if(agent.indexOf('safari') > -1) {
+            brs = 'Safari';
+        } else if(agent.indexOf('firefox') > -1) {
+            brs = 'Safari';
+        } else {
+            brs = '不明なブラウザ';
+        }
+    return "あなたは<font color='#FF0000'>" + brs + "</font>を使っています。";
+}
+
 let yobi = new Array("日","月","火","水","木","金","土");
 let today = new Date();
 let week = today.getDay();
