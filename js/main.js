6c7021fd3a2a8607985bea4d6855ad617cf8e9a8
yamanekojeez <yamaneko.jeez@gmail.com>
2020-02-29 12:05:00 +0900

    modified:   main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* alert("Hello World!"); */
-let yobi = new Array("日","月","火","水","木","金","土");
-let today = new Date();
-let week = today.getDay();
+var yobi = new Array("日","月","火","水","木","金","土");
+var today = new Date();
+var week = today.getDay();
 document.write( "本日は" + yobi[week] + "曜日!");
 
 function browser() {
         var brs;
-        let agent = window.navigator.userAegnt.toLowerCase();
+        var agent = window.navigator.userAegnt.toLowerCase();
         if(agent.indexOf('msie') > -1) {
             brs = 'InternetExplorer';
         } else if(agent.indexOf('edge') > -1) {
