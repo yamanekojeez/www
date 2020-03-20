d5ba41053c17e1c1583da598329d607266ea7cfb
yamanekojeez <yamaneko.jeez@gmail.com>
2020-03-20 14:52:43 +0900

    update main.js

--- js/main.js
+++ js/main.js
@@ -4,40 +4,38 @@ var today = new Date();
 var week = today.getDay();
 document.write( "本日は" + yobi[week] + "曜日!");
 
-function browser(){
-    var result = '不明';
-    var agent = window.navigator.userAgent.toLowerCase();
-    var version = window.navigator.appVersion.toLowerCase();
+function checkBrowser(){
+  var result = '不明';
+  var agent = window.navigator.userAgent.toLowerCase();
+  var version = window.navigator.appVersion.toLowerCase();
 
-    if(agent.indexOf("msie") > -1){
-        if (version.indexOf("msie 6.") > -1){
-            result = 'IE6';
-        }else if (version.indexOf("msie 7.") > -1){
-            result = 'IE7';
-        }else if (version.indexOf("msie 8.") > -1){
-            result = 'IE8';
-        }else if (version.indexOf("msie 9.") > -1){
-            result = 'IE9';
-        }else if (version.indexOf("msie 10.") > -1){
-            result = 'IE10';
-        }else{
-            result = 'IE(バージョン不明)';
-        }
-    }else if(agent.indexOf("trident/7") > -1){
-        result = 'IE11';
-    }else if(agent.indexOf("edge") > -1){
-        result = 'Edge';
-    }else if (agent.indexOf("chrome") > -1){
-        result = 'Chrome';
-    }else if (agent.indexOf("safari") > -1){
-        result = 'Safari';
-    }else if (agent.indexOf("opera") > -1){
-        result = 'Opera';
-    }else if (agent.indexOf("firefox") > -1){
-        result = 'Firefox';
+  if(agent.indexOf("msie") > -1){
+    if (version.indexOf("msie 6.") > -1){
+      result = 'IE6';
+    }else if (version.indexOf("msie 7.") > -1){
+      result = 'IE7';
+    }else if (version.indexOf("msie 8.") > -1){
+      result = 'IE8';
+    }else if (version.indexOf("msie 9.") > -1){
+      result = 'IE9';
+    }else if (version.indexOf("msie 10.") > -1){
+      result = 'IE10';
+    }else{
+      result = 'IE(バージョン不明)';
     }
-  document.write("使っているブラウザは" + result + "です");
+  }else if(agent.indexOf("trident/7") > -1){
+    result = 'IE11';
+  }else if(agent.indexOf("edge") > -1){
+    result = 'Edge';
+  }else if (agent.indexOf("chrome") > -1){
+    result = 'Chrome';
+  }else if (agent.indexOf("safari") > -1){
+    result = 'Safari';
+  }else if (agent.indexOf("opera") > -1){
+    result = 'Opera';
+  }else if (agent.indexOf("firefox") > -1){
+    result = 'Firefox';
+  }
+
+  alert("お使いのブラウザは「" + result + "」です。");
 }
-document.write( "使っているブラウザは" + result );
-var target = document.getElementById("view_brs").value;
-target.innerHTML = browser();
