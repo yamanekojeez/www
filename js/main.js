8531ef1a2e11c4e172974fbd4c1d7c8aa665182b
yamanekojeez <yamaneko.jeez@gmail.com>
2020-03-20 15:19:52 +0900

    update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,25 @@ var today = new Date();
 var week = today.getDay();
 document.write( "本日は" + yobi[week] + "曜日!");
 
+var userAgent = window.navigator.userAgent.toLowerCase();
+
+if(userAgent.indexOf('msie') != -1 ||
+        userAgent.indexOf('trident') != -1) {
+    document.write('Internet Explorerをお使いですね');
+} else if(userAgent.indexOf('edge') != -1) {
+    document.write('Edgeをお使いですね');
+} else if(userAgent.indexOf('chrome') != -1) {
+    document.write('Google Chromeをお使いですね');
+} else if(userAgent.indexOf('safari') != -1) {
+    document.write('Safariをお使いですね');
+} else if(userAgent.indexOf('firefox') != -1) {
+    document.write('FireFoxをお使いですね');
+} else if(userAgent.indexOf('opera') != -1) {
+    document.write('Operaをお使いですね');
+} else {
+    document.write('そんなブラウザは知らん');
+}
+
 function checkBrowser(){
     var result = '不明';
     var agent = window.navigator.userAgent.toLowerCase();
