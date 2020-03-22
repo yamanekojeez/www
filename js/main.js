d8b0583814163a10380b113792a760badb3a3bde
ヤマネコ <47523549+yamanekojeez@users.noreply.github.com>
2020-03-22 12:05:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,44 @@
-/* alert("Hello World!"); */
+/* js/main.js */
 var yobi = new Array("日", "月", "火", "水", "木", "金", "土");
 var today = new Date();
 var week = today.getDay();
 document.write("本日は" + yobi[week] + "曜日!<br>");
 
-var userAgent = window.navigator.userAgent.toLowerCase();
-if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("trident") != -1) {
-	document.write("Internet Explorerをお使いですね");
-} else if (userAgent.indexOf("edge") != -1) {
-	document.write("Edgeをお使いですね");
-} else if (userAgent.indexOf("chrome") != -1) {
-	document.write("Google Chromeをお使いですね");
-} else if (userAgent.indexOf("safari") != -1) {
-	document.write("Safariをお使いですね");
-} else if (userAgent.indexOf("firefox") != -1) {
-	document.write("FireFoxをお使いですね");
-} else if (userAgent.indexOf("opera") != -1) {
-	document.write("Operaをお使いですね");
-} else {
-	document.write("そんなブラウザは知らん");
+function checkBrowser() {
+	var result = '不明';
+	var agent = window.navigator.userAgent.toLowerCase();
+	var version = window.navigator.appVersion.toLowerCase();
+
+	if (agent.indexOf("msie") > -1) {
+		if (version.indexOf("msie 6.") > -1) {
+			result = 'IE6';
+		} else if (version.indexOf("msie 7.") > -1) {
+			result = 'IE7';
+		} else if (version.indexOf("msie 8.") > -1) {
+			result = 'IE8';
+		} else if (version.indexOf("msie 9.") > -1) {
+			result = 'IE9';
+		} else if (version.indexOf("msie 10.") > -1) {
+			result = 'IE10';
+		} else {
+			result = 'IE(バージョン不明)';
+		}
+	} else if (agent.indexOf("trident/7") > -1) {
+		result = 'IE11';
+	} else if (agent.indexOf("edge") > -1) {
+		result = 'Edge';
+	} else if (agent.indexOf("chrome") > -1) {
+		result = 'Chrome';
+	} else if (agent.indexOf("safari") > -1) {
+		result = 'Safari';
+	} else if (agent.indexOf("opera") > -1) {
+		result = 'Opera';
+	} else if (agent.indexOf("firefox") > -1) {
+		result = 'Firefox';
+	}
+	alert("お使いのブラウザは「" + result + "」です。");
 }
 
-function ChangeParaToDate(){
-	document.getElementById("eid_date").innerHTML = Date();
+function ChangeParaToDate() {
+	document.getElementById("eid_date").innerHTML = checkBrowser();
 }
-
