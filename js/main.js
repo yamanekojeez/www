3d0c9f551913a2e2348e3c6b194417a4a4533a68
yamanekojeez <yamaneko.jeez@gmail.com>
2020-03-22 09:21:21 +0900

    modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -21,36 +21,7 @@ if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("trident") != -1) {
 	document.write("そんなブラウザは知らん");
 }
 
-function checkBrowser() {
-	var result = "不明";
-	var agent = window.navigator.userAgent.toLowerCase();
-	var version = window.navigator.appVersion.toLowerCase();
-	if (agent.indexOf("msie") > -1) {
-		if (version.indexOf("msie 6.") > -1) {
-			result = "IE6";
-		} else if (version.indexOf("msie 7.") > -1) {
-			result = "IE7";
-		} else if (version.indexOf("msie 8.") > -1) {
-			result = "IE8";
-		} else if (version.indexOf("msie 9.") > -1) {
-			result = "IE9";
-		} else if (version.indexOf("msie 10.") > -1) {
-			result = "IE10";
-		} else {
-			result = "IE(バージョン不明)";
-		}
-	} else if (agent.indexOf("trident/7") > -1) {
-		result = "IE11";
-	} else if (agent.indexOf("edge") > -1) {
-		result = "Edge";
-	} else if (agent.indexOf("chrome") > -1) {
-		result = "Chrome";
-	} else if (agent.indexOf("safari") > -1) {
-		result = "Safari";
-	} else if (agent.indexOf("opera") > -1) {
-		result = "Opera";
-	} else if (agent.indexOf("firefox") > -1) {
-		result = "Firefox";
-	}
-	document.write("お使いのブラウザは「" + result + "」です。");
+function ChangeParaToDate(){
+	document.getElementByID("eid_date").innerHTML = Date();
 }
+
