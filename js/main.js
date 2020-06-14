ab52a4a2f7adfe77f8fd5ab78a0845633f69b53b
ヤマネコ <47523549+yamanekojeez@users.noreply.github.com>
2020-06-14 10:44:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,5 +43,5 @@ function ChangeParaToDate() {
 	document.getElementById("browser").innerHTML = checkBrowser();
 }
 function alt() {
-	document.write("ようこそ艦隊司令部へ！\n提督を応援しています。");
+	alart("ようこそ艦隊司令部へ！\n提督を応援しています。");
 }
